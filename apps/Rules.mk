a3defc4fa5af6867a099397b8a7c4db5bfd66de1
Arthur Amezcua <art.amezcua@stanford.edu>
2023-02-15 12:44:03 -0800

    Removes superfluous REGISTER_APPLICATION macro calls Fixes a couple of typos; fixes harps to some degree

--- apps/Rules.mk
+++ apps/Rules.mk
@@ -4,55 +4,53 @@ dirstack_$(sp)	:= $(d)
 d				:= $(dir)
 
 # Local variables
-PROJECT_$(d)				:= $(call GET_PROJECT,$(d))
-CEXE_$(d)					:= $(addprefix $(d)/, GetNextID)
-APPLICATIONS_$(d)			:= $(CEXE_$(d))
-$(foreach application,$(APPLICATIONS_$(d)),$(call REGISTER_APPLICATION,$(PROJECT_$(d)),$(notdir $(application)),$(application)))
-APPLICATION_TARGETS_$(d)	:= $(foreach application,$(notdir $(APPLICATIONS_$(d))),$(PROJECT_$(d))_$(application))
+PROJECT_$(d)					:= $(call GET_PROJECT,$(d))
+CEXE_$(d)						:= $(addprefix $(d)/, GetNextID)
+APPLICATIONS_$(d)				:= $(CEXE_$(d))
+APPLICATION_TARGETS_$(d)		:= $(foreach application,$(notdir $(APPLICATIONS_$(d))),$(PROJECT_$(d))_$(application))
 
 .PHONY:	$(APPLICATION_TARGETS_$(d))
 $(call MAKE_BUILT_APPLICATION_PREREQS,$(APPLICATION_TARGETS_$(d)))
 
-$(PROJECT_$(d))_all::			$(APPLICATION_TARGETS_$(d))
-install_$(PROJECT_$(d))_all::	$(foreach application_target,$(APPLICATION_TARGETS_$(d)),install_$(application_target))
+$(PROJECT_$(d))_all::			   $(APPLICATION_TARGETS_$(d))
+install_$(PROJECT_$(d))_all::	   $(foreach application_target,$(APPLICATION_TARGETS_$(d)),install_$(application_target))
 
-PROJ_CEXE	:= $(PROJ_CEXE) $(CEXE_$(d))
+PROJ_CEXE						:= $(PROJ_CEXE) $(CEXE_$(d))
 
-OBJ_$(d)	:= $(APPLICATIONS_$(d):%=%.o) 
-DEP_$(d)	:= $(OBJ_$(d):%=%.d)
-CLEAN		:= $(CLEAN) \
-		$(OBJ_$(d)) \
-		$(APPLICATIONS_$(d)) \
-		$(DEP_$(d))
+OBJ_$(d)						:= $(APPLICATIONS_$(d):%=%.o) 
+DEP_$(d)						:= $(OBJ_$(d):%=%.d)
+CLEAN							:= $(CLEAN) \
+								   $(OBJ_$(d)) \
+								   $(APPLICATIONS_$(d)) \
+								   $(DEP_$(d))
 
-S_$(d)		:= $(notdir $(APPLICATIONS_$(d)))
-
-TGT_BIN		:= $(TGT_BIN) $(APPLICATIONS_$(d))
+S_$(d)							:= $(notdir $(APPLICATIONS_$(d)))
+TGT_BIN							:= $(TGT_BIN) $(APPLICATIONS_$(d))
 
 # exe/lib-specific
-BUILD_DEPENDENCIES_$(d)		:=
-HEADER_DEPENDENCIES_$(d)	:=
-DEPENDENCY_LIST_$(d)		:= $(BUILD_DEPENDENCIES_$(d)) $(HEADER_DEPENDENCIES_$(d))
-LINK_LIBS_FLAGS_$(d)		:= $(call GEN_LINK_LIBS_FLAGS,$(BUILD_DEPENDENCIES_$(d)))
-INCS_FLAGS_$(d)				:= $(call GEN_INC_FLAGS,$(DEPENDENCY_LIST_$(d)))
-HEADER_PREREQS_$(d)			:= $(call GEN_HEADER_PREREQS,$(DEPENDENCY_LIST_$(d)))
-DEPENDENCY_PREREQS_$(d)		:= $(call GEN_DEPENDENCY_PREREQS,$(BUILD_DEPENDENCIES_$(d)))
+BUILD_DEPENDENCIES_$(d)			:=
+HEADER_DEPENDENCIES_$(d)		:=
+DEPENDENCY_LIST_$(d)			:= $(BUILD_DEPENDENCIES_$(d)) $(HEADER_DEPENDENCIES_$(d))
+LINK_LIBS_FLAGS_$(d)			:= $(call GEN_LINK_LIBS_FLAGS,$(BUILD_DEPENDENCIES_$(d)))
+INCS_FLAGS_$(d)					:= $(call GEN_INC_FLAGS,$(DEPENDENCY_LIST_$(d)))
+HEADER_PREREQS_$(d)				:= $(call GEN_HEADER_PREREQS,$(DEPENDENCY_LIST_$(d)))
+DEPENDENCY_PREREQS_$(d)			:= $(call GEN_DEPENDENCY_PREREQS,$(BUILD_DEPENDENCIES_$(d)))
 
 # Local rules
-$(OBJ_$(d)):	$(SRCDIR)/$(d)/Rules.mk $(HEADER_PREREQS_$(d))
-$(OBJ_$(d)):	CF_TGT := $(CF_TGT) $(INCS_FLAGS_$(d)) -I$(SRCDIR)/$(d)/src/
+$(OBJ_$(d)):					   $(SRCDIR)/$(d)/Rules.mk $(HEADER_PREREQS_$(d))
+$(OBJ_$(d)):					   CF_TGT := $(CF_TGT) $(INCS_FLAGS_$(d)) -I$(SRCDIR)/$(d)/src/
 
-$(APPLICATIONS_$(d)):	LL_TGT := $(LL_TGT) $(LINK_LIBS_FLAGS_$(d))
-$(APPLICATIONS_$(d)):	PREREQS := $(PREREQS) $(DEPENDENCY_PREREQS_$(d))
-$(APPLICATIONS_$(d)):	$(DEPENDENCY_PREREQS_$(d))
+$(APPLICATIONS_$(d)):			   LL_TGT := $(LL_TGT) $(LINK_LIBS_FLAGS_$(d))
+$(APPLICATIONS_$(d)):			   PREREQS := $(PREREQS) $(DEPENDENCY_PREREQS_$(d))
+$(APPLICATIONS_$(d)):			   $(DEPENDENCY_PREREQS_$(d))
 
 .PHONY:	$(foreach application_target,$(APPLICATION_TARGETS_$(d)),install_$(application_target) install_$(application_target)_bins)
 $(foreach application_target,$(APPLICATION_TARGETS_$(d)),install_$(application_target)):	install_%:	install_%_bins
-INSTALLED_BINS_$(d) := $(addprefix $(BINS_DIR_$(PROJECT_$(d)))/, $(notdir $(foreach application_target,$(APPLICATION_TARGETS_$(d)),$($(application_target)))))
+INSTALLED_BINS_$(d)				:= $(addprefix $(BINS_DIR_$(PROJECT_$(d)))/, $(notdir $(foreach application_target,$(APPLICATION_TARGETS_$(d)),$($(application_target)))))
 
 $(call MAKE_INSTALLED_APPLICATION_PREREQS,$(APPLICATION_TARGETS_$(d)))
-$(INSTALLED_BINS_$(d)):	$(BINS_DIR_$(PROJECT_$(d)))/%:	$(d)/%
-					cp $< $@
+$(INSTALLED_BINS_$(d)):			   $(BINS_DIR_$(PROJECT_$(d)))/%:	$(d)/%
+								   cp $< $@
 
 # Shortcuts
 .PHONY:		$(S_$(d))
